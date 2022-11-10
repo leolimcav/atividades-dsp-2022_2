5c15dfb56e1af2465bd0d5e4a6759012a42f37e0
Leonardo Lima Cavalcante <leonardo123k@gmail.com>
2022-11-10 03:03:52 -0300

    update movierepository

--- trabalho2/src/main/java/br/quixada/ufc/trabalho2/repositories/MovieRepository.java
+++ trabalho2/src/main/java/br/quixada/ufc/trabalho2/repositories/MovieRepository.java
@@ -13,12 +13,12 @@ public interface MovieRepository extends CrudRepository<Movie, Integer> {
   List<Movie> findAllByTitle(String title);
 
   // c) Listar os nomes de todos os atores de um determinado filme.
-  @Query(value = "SELECT a.name FROM Actor a, Movie m, movie_actor ma WHERE ma.movie_id = m.id AND ma.actor_id = a.id AND m.id = :id", nativeQuery = true)
-  List<String> findAllActorNamesWhereMovieIdIsEqualTo(Integer id);
+  @Query(value = "SELECT a FROM Actor a, Movie m, movie_actor ma WHERE ma.movie_id = m.id AND ma.actor_id = a.id AND m.id = :id", nativeQuery = true)
+  List<Movie> findAllActorNamesWhereMovieIdIsEqualTo(Integer id);
 
   // d) Listar os títulos de filmes lançados em determinado ano.
-  List<String> findAllMovieNameByReleaseYear(Integer year);
+  List<Movie> findByReleaseYear(Integer year);
 
   // e) Listar os títulos de filmes cujo título contém determinada string.
-  List<String> findAllMovieTitleByTitleLike(String title);
+  List<Movie> findAllByTitleLike(String title);
 }
